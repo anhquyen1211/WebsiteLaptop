855d2504f8c95f16a0501e922592aaeec8b2d668
QuyenLe <leanhquyen1211@gmail.com>
2023-05-04 18:42:22 +0700

    Dieu huong giao dien

---
+++ WebBanLaptop/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace WebBanLaptop.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        //đăng xuất admin quay về trang chủ
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("~/Home/Index");
+        }
+        //chuyển từ trang admin sang trang thông tin cá nhân
+        public ActionResult ViewProfile()
+        {
+            return Redirect("~/Account/Editprofile");
+        }
+        //chuyển từ trang admin sang trang chủ
+        public ActionResult BackToHome()
+        {
+            return Redirect("~/Home/Index");
+        }
+    }
+}
