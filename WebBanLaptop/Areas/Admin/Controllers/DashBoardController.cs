c3bb5ae04d2a6bdb39efe1df7a7308e3b0a7bf0c
QuyenLe <leanhquyen1211@gmail.com>
2023-05-06 10:38:17 +0700

    thong ke

--- WebBanLaptop/Areas/Admin/Controllers/DashBoardController.cs
+++ WebBanLaptop/Areas/Admin/Controllers/DashBoardController.cs
@@ -8,9 +8,14 @@ namespace WebBanLaptop.Areas.Admin.Controllers
 {
     public class DashBoardController : Controller
     {
+        private LQShopDb db = new LQShopDb();
         // GET: Admin/DashBoard
         public ActionResult Index()
         {
+            ViewBag.Order = db.Orders.Where(m => m.Status == "3").ToList();
+            ViewBag.OrderDetail = db.Order_Detail.ToList();
+            ViewBag.ListOrderDetail = db.Order_Detail.Where(m => m.Order.Status == "3").OrderByDescending(m => m.Create_at).Take(3).ToList();
+            ViewBag.ListOrder = db.Orders.OrderByDescending(m => m.Create_at).Take(7).ToList();
             return View();
         }
     }
